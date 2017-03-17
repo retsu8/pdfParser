0d8341c087316f9676835a6970126059483dd5cf
WJP <wjp@frontlineprocessing.com>
2017-03-17 16:26:35 -0600

    fixing up font size and concept

--- pdfAddition.rb
+++ pdfAddition.rb
@@ -6,6 +6,7 @@ require 'prawn'
 require 'pdftk'
 require 'combine_pdf'
 require 'fileutils'
+require 'date'
 
 $creditcards =['discover','amex','mastercard','visa']
 Money.use_i18n = false
@@ -26,7 +27,7 @@ if $list.to_s.empty?
   exit
 end
 def buildUsecase
-  $date = $time.strftime("%d/%m/%Y")
+  $date = $time.strftime("%d-%m-%Y")
   $dollaramount = "$"+ Money.new($list[1], 'USD').to_s
   $name = $list[2]
   $address = $list[3]
@@ -38,7 +39,8 @@ def buildUsecase
   $reasoncode = $list[9]
   $message = $list[10]
   $addcomments = $list[11]
-  $transDate = $list[12]
+  $dcharg =  DateTime.parse($list[12])
+  $transDate = $dcharg.strftime("%d-%m-%Y")
   $cardnumber = $list[13]
   $transamount = "$"+ Money.new($list[14], 'USD').to_s
   $ref = $list[15]
@@ -48,24 +50,34 @@ def buildUsecase
 end
 def prawnbuild
   Prawn::Document.generate($prawnmask, :page_size => "LETTER", :page_layout => :portrait) do
-    draw_text $date, :at => [85,630]
-    draw_text $dollaramount, :at =>[425,630]
-    draw_text $name.capitalize+",", :at =>[50,610]
-    draw_text $address.capitalize+",", :at =>[50,595]
-    draw_text $city.capitalize+", "+$state, :at =>[50,580]
-    draw_text $zip, :at =>[50,565]
-    text_box $chargeamount, :at =>[410,260], :size => 10, :height => 100, :width => 200, :alight => :right
-    text_box $totalamount, :at =>[410,235],  :size => 10, :height => 100, :width => 150, :alight => :right
-    text_box $reasoncode, :at =>[140,193],  :size => 10
-    text_box $message, :at =>[140,180],  :size => 10
-    text_box $addcomments, :at =>[140,180],  :size => 10
-    text_box $transDate, :at =>[170,125],  :size => 10
-    text_box $cardnumber, :at =>[220,112],  :size => 10
-    text_box $transamount, :at =>[220,100],  :size => 10
-    text_box $ref, :at =>[225,86],  :size => 10
-    text_box $pos, :at =>[162,73],  :size => 10
-    text_box $trancode, :at =>[360,73],  :size => 10
-    text_box $reccode, :at =>[148,61],  :size => 10
+    font "Courier" do
+      draw_text $date, :at => [85,630]
+      draw_text $dollaramount, :at =>[425,630]
+      draw_text $name.capitalize+",", :at =>[50,590]
+      draw_text $address.capitalize+",", :at =>[50,575]
+      draw_text $city.capitalize+", "+$state+",", :at =>[50,560]
+      draw_text $zip +",", :at =>[50,545]
+      font_size 10
+      w = 57
+      bounding_box [400,260], :width => w, :height => 10 do
+        #stroke_bounds
+        text_box $chargeamount, :width => w, :align => :right
+      end
+      bounding_box [400,235], :width => w, :height => 10 do
+        #stroke_bounds
+        text_box $totalamount, :width => w, :align => :right
+      end
+      text_box $reasoncode, :at =>[140,193]
+      text_box $message, :at =>[140,180]
+      text_box $addcomments, :at =>[140,180]
+      text_box $transDate, :at =>[170,125]
+      text_box $cardnumber, :at =>[220,112]
+      text_box $transamount, :at =>[220,100]
+      text_box $ref, :at =>[225,86]
+      text_box $pos, :at =>[162,73]
+      text_box $trancode, :at =>[360,73]
+      text_box $reccode, :at =>[148,61]
+    end
   end
 end
 
@@ -73,8 +85,9 @@ def merge(type)
   stamp = CombinePDF.load($prawnmask).pages[0]
   pdf = CombinePDF.load(type) # one way to combine, very fast.
   pdf.pages.each {|page| page << stamp}
-  FileUtils::mkdir_p "chargebackPDF"+"/"+$mid
-  pdf.save "chargebackPDF"+"/"+$mid+"/"+type+$ref+$cardnumber+$transamount+".pdf"
+  $month = Date::MONTHNAMES[$dcharg.month]
+  FileUtils::mkdir_p "chargebackPDF/"+$mid+"/"+$dcharg.year.to_s+"/"+$month.to_s
+  pdf.save "chargebackPDF/"+$mid+"/"+$dcharg.year.to_s+"/"+$month.to_s+"/card"+$cardnumber+"reference"+$ref+".pdf"
 end
 matcher = FuzzyMatch.new($creditcards)
 matched = matcher.find($list[0])
